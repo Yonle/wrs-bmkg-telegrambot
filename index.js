b2557589b5c3b0345cc90521567cb4a192292319
Yonle <yonle@protonmail.com>
2022-01-14 19:52:57 +0700

    Format kode

--- index.js
+++ index.js
@@ -8,36 +8,52 @@ require("dotenv").config();
 const bot = new grammy.Bot(process.env.BOT_TOKEN);
 const subscriber = [];
 
-bot.command("start", async ctx => {
-  if (!subscriber.includes(ctx.message.chat.id)) subscriber.push(ctx.message.chat.id);
+bot.command("start", async (ctx) => {
+  if (!subscriber.includes(ctx.message.chat.id))
+    subscriber.push(ctx.message.chat.id);
   bot.api.sendChatAction(ctx.message.chat.id, "upload_photo");
-  await ctx.replyWithPhoto(new grammy.InputFile(
-    get(`https://data.bmkg.go.id/DataMKG/TEWS/${wrs.lastAlert.info.shakemap}`)),
+  await ctx.replyWithPhoto(
+    new grammy.InputFile(
+      get(`https://data.bmkg.go.id/DataMKG/TEWS/${wrs.lastAlert.info.shakemap}`)
+    ),
     {
       caption: `*${wrs.lastAlert.info.subject}*\n\n${wrs.lastAlert.info.description}\n\n${wrs.lastAlert.info.headline}\n\n⚠️${wrs.lastAlert.info.instruction}`,
       parse_mode: "Markdown",
       reply_markup: {
-        inline_keyboard: [[{
-          text: "Buka WRS-BMKG",
-	  url: "https://warning.bmkg.go.id"
-	}]]
-      }
+        inline_keyboard: [
+          [
+            {
+              text: "Buka WRS-BMKG",
+              url: "https://warning.bmkg.go.id",
+            },
+          ],
+        ],
+      },
     }
   );
-  let text = `*${wrs.lastRealtimeQL.properties.place}*\``
+  let text = `*${wrs.lastRealtimeQL.properties.place}*\``;
   text += `\nWaktu     : ${new Date(
     wrs.lastRealtimeQL.properties.time
   ).toLocaleTimeString("us", { timeZone: "Asia/Jakarta" })}`;
-  text += `\nMagnitude : ${Number(wrs.lastRealtimeQL.properties.mag).toFixed(2)} M`;
+  text += `\nMagnitude : ${Number(wrs.lastRealtimeQL.properties.mag).toFixed(
+    2
+  )} M`;
   text += `\nFase      : ${wrs.lastRealtimeQL.properties.fase}`;
   text += `\nStatus    : ${wrs.lastRealtimeQL.properties.status}`;
-  text += `\nKedalaman : ${Math.floor(wrs.lastRealtimeQL.properties.depth)} KM\``;
+  text += `\nKedalaman : ${Math.floor(
+    wrs.lastRealtimeQL.properties.depth
+  )} KM\``;
   let locationMessage = await bot.api.sendVenue(
     ctx.message.chat.id,
     wrs.lastRealtimeQL.geometry.coordinates[1],
     wrs.lastRealtimeQL.geometry.coordinates[0],
     wrs.lastRealtimeQL.properties.place,
-    Number(wrs.lastRealtimeQL.properties.mag).toFixed(2) + " M, " + new Date(wrs.lastRealtimeQL.properties.mag).toLocaleDateString("id", { timeZone: "Asia/Jakarta" }) + " (WIB)"
+    Number(wrs.lastRealtimeQL.properties.mag).toFixed(2) +
+      " M, " +
+      new Date(wrs.lastRealtimeQL.properties.mag).toLocaleDateString("id", {
+        timeZone: "Asia/Jakarta",
+      }) +
+      " (WIB)"
   );
   await ctx.reply(text, {
     parse_mode: "Markdown",
@@ -53,17 +69,21 @@ wrs.on("Gempabumi", (msg) => {
     await bot.api.sendPhoto(
       id,
       new grammy.InputFile(
-	      get(`https://data.bmkg.go.id/DataMKG/TEWS/${msg.shakemap}`)
-	),
+        get(`https://data.bmkg.go.id/DataMKG/TEWS/${msg.shakemap}`)
+      ),
       {
         caption: `*${msg.subject}*\n\n${msg.description}\n\n${msg.headline}\n\n*${msg.instruction}*`,
         parse_mode: "Markdown",
-	reply_markup: {
-          inline_keyboard: [[{
-	    text: "Buka WRS-BMKG",
-            url: "https://warning.bmkg.go.id"
-          }]]
-        }
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Buka WRS-BMKG",
+                url: "https://warning.bmkg.go.id",
+              },
+            ],
+          ],
+        },
       }
     );
   });
@@ -71,10 +91,13 @@ wrs.on("Gempabumi", (msg) => {
 
 wrs.on("realtime", (msg) => {
   if (wrs.recvWarn !== 2) return wrs.recvWarn++;
-  let text = `*${wrs.lastRealtimeQL.properties.place}*\``
-  text += `\nTanggal   : ${new Date(msg.properties.time).toLocaleDateString("id", {
-    timeZone: "Asia/Jakarta",
-  })}`;
+  let text = `*${wrs.lastRealtimeQL.properties.place}*\``;
+  text += `\nTanggal   : ${new Date(msg.properties.time).toLocaleDateString(
+    "id",
+    {
+      timeZone: "Asia/Jakarta",
+    }
+  )}`;
   text += `\nMagnitude : ${Number(msg.properties.mag).toFixed(2)} M`;
   text += `\nFase      : ${msg.properties.fase}`;
   text += `\nStatus    : ${msg.properties.status}`;
@@ -86,7 +109,12 @@ wrs.on("realtime", (msg) => {
       msg.geometry.coordinates[1],
       msg.geometry.coordinates[0],
       wrs.lastRealtimeQL.properties.place,
-      Number(msg.properties.mag).toFixed(2) + " M, " + new Date(msg.properties.time).toLocaleTimeString("us", { timeZone: "Asia/Jakarta" }) + " (WIB)"
+      Number(msg.properties.mag).toFixed(2) +
+        " M, " +
+        new Date(msg.properties.time).toLocaleTimeString("us", {
+          timeZone: "Asia/Jakarta",
+        }) +
+        " (WIB)"
     );
     await bot.api.sendMessage(id, text, {
       parse_mode: "Markdown",
@@ -97,7 +125,9 @@ wrs.on("realtime", (msg) => {
 
 wrs.startPolling();
 
-bot.start()
-bot.api.getMe().then(({ username }) => console.log(`Berhasil masuk sebagai @${username}`));
-bot.catch(e => console.error(e));
+bot.start();
+bot.api
+  .getMe()
+  .then(({ username }) => console.log(`Berhasil masuk sebagai @${username}`));
+bot.catch((e) => console.error(e));
 process.on("unhandledRejection", console.error);
